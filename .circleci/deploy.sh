782f819bc3e0b4da92b2115fe9598d33ec9af661
nabosh <nabosh@gmail.com>
2023-03-27 13:37:38 -0600

    trying to fix errorrrrrrrrrsssssssss

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -26,9 +26,6 @@ LAMBDA_VERSION=$(aws lambda publish-version \
   --query Version \
   --output text)
 
-# Export the LAMBDA_VERSION variable
-export LAMBDA_VERSION
-
 # Update CloudFront behavior
 DISTRIBUTION_ID=E380M7BHVXFP6X
 DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
@@ -37,6 +34,11 @@ BEHAVIOR_PATH_PATTERN="*"
 
 # Call the deploy.mjs script and pass the required arguments
 node deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN"
+
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation \
+  --distribution-id DISTRIBUTION_ID \
+  --paths "/*"
 
 # Sync directory with S3 bucket
 aws s3 sync dist/circlecitest s3://circleciangularbucket --delete
