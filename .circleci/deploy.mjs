0b1d4bc1d3f0e8f812eb010db55af5371a8a9dfc
nabosh <nabosh@gmail.com>
2023-03-27 11:53:53 -0600

    breaking out config.yml

---
+++ .circleci/deploy.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import { exec } from 'child_process';
+
+const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern] = process.argv;
+
+const updateCloudFrontBehavior = () => {
+  exec(
+    `jq --arg lambda_version "${lambdaVersion}" 'del(.ETag) | .DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Quantity = 1 | .DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN = "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)" | .DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].EventType = "origin-response" | .DistributionConfig' distribution-config-original.json > distribution-config-updated.json`,
+    (error) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      exec(
+        `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
+        (error) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log('CloudFront behavior updated');
+        }
+      );
+    }
+  );
+};
+
+updateCloudFrontBehavior();
---
+++ .circleci/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Deploy Angular App
+
+# Install AWS CLI
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo pip3 install awscli
+
+# Configure AWS CLI
+aws configure set default.region us-east-1
+
+# Zip lambda file
+zip -q src/aws/lambda.zip src/aws/lambda.mjs
+
+# Deploy Lambda function
+aws lambda update-function-code \
+  --function-name header-lambda \
+  --zip-file fileb://src/aws/lambda.zip
+
+# Wait for Lambda update to complete
+sleep 20
+
+# Publish Lambda version
+LAMBDA_VERSION=$(aws lambda publish-version \
+  --function-name header-lambda \
+  --query Version \
+  --output text)
+
+# Update CloudFront behavior
+DISTRIBUTION_ID=E380M7BHVXFP6X
+DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
+aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
+BEHAVIOR_PATH_PATTERN="*"
+
+# Call the deploy.mjs script and pass the required arguments
+node deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN"
+
+# Sync directory with S3 bucket
+aws s3 sync dist/circlecitest s3://circleciangularbucket --delete
