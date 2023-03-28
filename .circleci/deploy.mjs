c6df8c687059396d5b38a5a90c0b72c3008807aa
nabosh <nabosh@gmail.com>
2023-03-27 21:34:05 -0600

    sufferinggggggggggggg

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -13,23 +13,22 @@ const updateDistributionConfig = (distributionConfig) => {
     console.log('temp-distribution-config.json content:', fs.readFileSync('temp-distribution-config.json', 'utf-8'));
   
     execSync(
-      `jq --arg lambda_version "${lambdaVersion}" '
-        . |= (
-          del(.ETag) |
-          .DefaultCacheBehavior.LambdaFunctionAssociations |=
-            if .Items == null then
-              .Items = [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}] |
-              .Quantity = 1
-            else
-              (.Quantity = (.Items | length + 1)) |
-              (.Items |= . + [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}])
-            end
-        )
-      ' temp-distribution-config.json > distribution-config-updated.json`,
-      { stdio: 'inherit' }
-    );
-  };
-  
+        `jq --arg lambda_version "${lambdaVersion}" "\
+          . |= (\
+            del(.ETag) |\
+            .DefaultCacheBehavior.LambdaFunctionAssociations |=\
+              if .Items == null then\
+                .Items = [{\\"EventType\\": \\"origin-response\\", \\"LambdaFunctionARN\\": \\"arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\\($lambda_version)\\"}] |\
+                .Quantity = 1\
+              else\
+                (.Quantity = (.Items | length + 1)) |\
+                (.Items |= . + [{\\"EventType\\": \\"origin-response\\", \\"LambdaFunctionARN\\": \\"arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\\($lambda_version)\\"}])\
+              end\
+          )\
+        " temp-distribution-config.json > distribution-config-updated.json`,
+        { stdio: 'inherit' }
+      );
+        
     
 const updateCloudFront = () => {
   execSync(
