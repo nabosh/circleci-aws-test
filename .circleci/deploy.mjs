2bff053042697077a32976b7650018ee8217baf7
nabosh <nabosh@gmail.com>
2023-03-28 10:46:09 -0600

    fix behavior level version update

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -3,33 +3,33 @@ import { execSync } from 'child_process';
 
 const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
 
-const updateDistributionConfig = (distConfig) => {
+const updateDistributionConfig = (distributionConfig) => {
     // Save DistributionConfig to a file
     execSync(
-      `echo '${JSON.stringify(distConfig.DistributionConfig)}' > temp-distribution-config.json`,
+      `echo '${JSON.stringify(distributionConfig.DistributionConfig)}' > temp-distribution-config.json`,
       { stdio: 'inherit' }
     );
-
+  
     console.log('temp-distribution-config.json content:', fs.readFileSync('temp-distribution-config.json', 'utf-8'));
-
+  
     execSync(
-        `jq --arg lambda_version "${lambdaVersion}" "\
-          . |= (\
-            del(.ETag) |\
-            .DefaultCacheBehavior.LambdaFunctionAssociations |=\
-              if .Items == null then\
-                .Items = [{\\"EventType\\": \\"origin-response\\", \\"LambdaFunctionARN\\": \\"arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\\($lambda_version)\\"}] |\
-                .Quantity = 1\
-              else\
-                (.Quantity = (.Items | length + 1)) |\
-                (.Items |= . + [{\\"EventType\\": \\"origin-response\\", \\"LambdaFunctionARN\\": \\"arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\\($lambda_version)\\"}])\
-              end\
-          )\
-        " temp-distribution-config.json > distribution-config-updated.json`,
-        { stdio: 'inherit' }
+      `jq --arg lambda_version "${lambdaVersion}" '
+        . |= (
+          del(.ETag) |
+          .DefaultCacheBehavior.LambdaFunctionAssociations |=
+            if .Items == null then
+              .Items = [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}] |
+              .Quantity = 1
+            else
+              (.Quantity = (.Items | length + 1)) |
+              (.Items |= . + [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}])
+            end
+        )
+      ' temp-distribution-config.json > distribution-config-updated.json`,
+      { stdio: 'inherit' }
     );
-};
-    
+  };
+      
 const updateCloudFront = () => {
     execSync(
         `aws cloudfront update-distribution \
