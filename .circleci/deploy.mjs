820d86f496d5b8ab7361af9dff60ee6983e1a35c
nabosh <nabosh@gmail.com>
2023-03-27 20:51:16 -0600

    sufferingggg

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -3,28 +3,24 @@ import { execSync } from 'child_process';
 const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
 
 const updateDistributionConfig = () => {
-  execSync(
-    `jq --arg lambda_version "${lambdaVersion}" '
-      del(.ETag) |
-      .DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations |=
-        if .Items == null then
-          .Items = [] |
-          .Quantity = 0
-        else
-          (.Quantity = (.Items | length)) |
-          (.Items |= map(
-            if .EventType == "origin-response" then
-              .LambdaFunctionARN = "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"
+    execSync(
+      `jq --arg lambda_version "${lambdaVersion}" '
+        .DistributionConfig |= (
+          del(.ETag) |
+          .DefaultCacheBehavior.LambdaFunctionAssociations |=
+            if .Items == null then
+              .Items = [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}] |
+              .Quantity = 1
             else
-              .
+              (.Quantity = (.Items | length + 1)) |
+              (.Items |= . + [{"EventType": "origin-response", "LambdaFunctionARN": "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"}])
             end
-          ))
-        end
-    ' ${configFile} > distribution-config-updated.json`,
-    { stdio: 'inherit' }
-  );
-};
-
+        )
+      ' ${configFile} > distribution-config-updated.json`,
+      { stdio: 'inherit' }
+    );
+  };
+  
 const updateCloudFront = () => {
   execSync(
     `aws cloudfront update-distribution \
