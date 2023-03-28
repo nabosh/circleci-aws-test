1613879249a0ac53d86eeefc2a457e46fcfe5927
nabosh <nabosh@gmail.com>
2023-03-28 14:28:40 -0600

    fix behavior level version update89

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -1,62 +1,37 @@
-// const { promises: fs } = require('fs');
-// const fs = require('fs').promises;
-import fs from "fs"
-const { spawn } = require('child_process');
-const AWS = require('aws-sdk');
+import { promises as fs } from 'fs';
+import { execSync } from 'child_process';
+import AWS from 'aws-sdk';
 
-const cloudfront = new AWS.CloudFront();
-const lambda = new AWS.Lambda();
-
-async function updateCloudFront() {
-  const functionName = process.env.LAMBDA_FUNCTION_NAME;
-  const functionInfo = await getLambdaFunctionInfo(functionName);
-  const distributionID = process.env.CLOUDFRONT_DISTRIBUTION_ID;
-  const distributionConfig = await getDistributionConfig(distributionID);
-  const lambdaARN = functionInfo.FunctionArn;
-
-  // Find the default cache behavior and update its Lambda function association
-  const defaultCacheBehavior = distributionConfig.DefaultCacheBehavior;
-  defaultCacheBehavior.LambdaFunctionAssociations = {
-    Quantity: 1,
-    Items: [
-      {
-        LambdaFunctionARN: lambdaARN,
-        EventType: 'viewer-request',
-      },
-    ],
-  };
-
-  await updateDistribution(distributionID, distributionConfig);
-}
-
-async function getDistributionConfig(distributionID) {
-  const params = { Id: distributionID };
-  const { DistributionConfig, ETag } = await cloudfront.getDistributionConfig(params).promise();
-
-  return { DistributionConfig, ETag };
-}
-
-async function updateDistribution(distributionID, { DistributionConfig, ETag }) {
-  const params = {
-    Id: distributionID,
-    DistributionConfig,
-    IfMatch: ETag,
-  };
-  await cloudfront.updateDistribution(params).promise();
-}
-
-async function getLambdaFunctionInfo(functionName) {
-  const params = { FunctionName: functionName };
-  const functionInfo = await lambda.getFunction(params).promise();
-  return functionInfo.Configuration;
-}
-
-(async () => {
+const updateCloudFrontBehavior = async (lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile) => {
   try {
-    await updateCloudFront();
-    console.log('Successfully updated CloudFront configuration with the new Lambda ARN.');
+    const configJSON = await fs.readFile(configFile, 'utf-8');
+    const config = JSON.parse(configJSON);
+    const behavior = config.DistributionConfig.CacheBehaviors.Items.find(
+      (b) => b.PathPattern === behaviorPathPattern
+    );
+
+    if (!behavior) {
+      console.error(`No CacheBehavior found with PathPattern "${behaviorPathPattern}".`);
+      process.exit(1);
+    }
+
+    behavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN = behavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN.replace(
+      /:\d+$/,
+      `:${lambdaVersion}`
+    );
+
+    await fs.writeFile('distribution-config-updated.json', JSON.stringify(config, null, 2));
+
+    execSync(
+      `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
+      { stdio: 'inherit' }
+    );
   } catch (error) {
-    console.error('Error updating CloudFront configuration:', error.message);
+    console.error(`Error updating CloudFront behavior: ${error.message}`);
     process.exit(1);
   }
-})();
+};
+
+const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
+
+updateCloudFrontBehavior(lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile);
