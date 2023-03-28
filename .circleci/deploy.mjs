77632feda9a8ad43607ff07dd4ff7c88f5a4bc52
nabosh <nabosh@gmail.com>
2023-03-27 20:44:37 -0600

    sufferinggg

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -1,40 +1,39 @@
-#!/usr/bin/env node
-import fs from 'fs';
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 
 const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
 
-const updateCloudFrontBehavior = () => {
-  const cmd = `
-    jq --arg lambda_version "${lambdaVersion}" '
+const updateDistributionConfig = () => {
+  execSync(
+    `jq --arg lambda_version "${lambdaVersion}" '
       del(.ETag) |
       .DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations |=
-        (.Quantity = (.Items | length)) |
-        (.Items |= map(
-          if .EventType == "origin-response" then
-            .LambdaFunctionARN = "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"
-          else
-            .
-          end
-        ))
-    ' ${configFile} > distribution-config-updated.json`;
-
-  exec(cmd, (error) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    exec(
-      `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
-      (error) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log('CloudFront behavior updated');
-      }
-    );
-  });
+        if .Items == null then
+          .Items = [] |
+          .Quantity = 0
+        else
+          (.Quantity = (.Items | length)) |
+          (.Items |= map(
+            if .EventType == "origin-response" then
+              .LambdaFunctionARN = "arn:aws:lambda:us-east-1:671249171349:function:header-lambda:\($lambda_version)"
+            else
+              .
+            end
+          ))
+        end
+    ' ${configFile} > distribution-config-updated.json`,
+    { stdio: 'inherit' }
+  );
 };
 
-updateCloudFrontBehavior();
+const updateCloudFront = () => {
+  execSync(
+    `aws cloudfront update-distribution \
+      --id ${distributionId} \
+      --if-match ${distributionEtag} \
+      --distribution-config file://distribution-config-updated.json`,
+    { stdio: 'inherit' }
+  );
+};
+
+updateDistributionConfig();
+updateCloudFront();
