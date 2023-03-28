c2cdf36626e6c35e4dfa75188bcc479bfc721cfb
nabosh <nabosh@gmail.com>
2023-03-28 15:53:12 -0600

    adjusting the lambda for testing

--- src/aws/index.js
+++ src/aws/index.js
@@ -2,9 +2,9 @@ exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
   const headers = request.headers;
 
-  if (request.uri.includes('auth')) {
+  // if (request.uri.includes('auth')) {
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'SAMEORIGIN' }];
-  }
+  // }
 
   return request;
 };
