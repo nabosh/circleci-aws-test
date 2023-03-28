de1c2765c8f23c479bfb428682ce170306eac771
nabosh <nabosh@gmail.com>
2023-03-28 15:43:20 -0600

    chaging association to viewer request

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -9,11 +9,11 @@ const updateCloudFrontBehavior = async (lambdaVersion, distributionId, distribut
     const behavior = config.DistributionConfig.DefaultCacheBehavior;
 
     const lambdaAssociation = behavior.LambdaFunctionAssociations.Items.find(
-      (item) => item.EventType === 'origin-response'
+      (item) => item.EventType === 'viewer-request'
     );
 
     if (!lambdaAssociation) {
-      console.error('No LambdaFunctionAssociation found with EventType "origin-response".');
+      console.error('No LambdaFunctionAssociation found with EventType "viewer-request".');
       process.exit(1);
     }
 
