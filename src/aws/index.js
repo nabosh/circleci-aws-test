5d8b94b97ebf1eb7be3603375196e67a0ca4a2e2
nabosh <nabosh@gmail.com>
2023-04-06 20:44:28 -0600

    trying to adjust for the auth not working with the angular SPA

--- src/aws/index.js
+++ src/aws/index.js
@@ -2,20 +2,17 @@
 
 exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
-  const response = event.Records[0].cf.response;
-  const headers = response.headers; 
+  const headers = request.headers;
 
-  console.log('Requestt:', request);
-  console.log('Responsee:', response);
   console.log('Request URI:', request.uri);
   console.log('Checking if request URI contains "auth":', request.uri.includes('auth'));
 
-  // if (request.uri.includes('auth')) {
-  //   console.log('URI contains "auth", adding X-Frame-Options header');
+  if (request.uri.includes('auth')) {
+    console.log('URI contains "auth", adding X-Frame-Options header');
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'SAMEORIGIN' }];
-  // }
+  }
 
   console.log('Updated Headers:', headers);
 
-  return response;
+  return request;
 };
