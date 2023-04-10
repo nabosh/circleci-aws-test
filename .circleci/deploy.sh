eb2d1ebf98d9fc84937c09cf5d89a43fd8d427ab
nabosh <nabosh@gmail.com>
2023-04-10 17:00:02 -0600

    trying to fix cloudfront update issueeeeeeeeeeee

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,74 +1,57 @@
-#!/bin/bash
-set -e
-# Deploy Angular App
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+import { promises as fs } from 'fs';
+import { execSync } from 'child_process';
 
-function create_lambda_package() {
-  # Create header-lambda directory
-  mkdir header-lambda
+const updateCloudFrontBehavior = async (lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile) => {
+  const configJSON = await fs.readFile(configFile, 'utf-8');
+  const config = JSON.parse(configJSON);
+  const behavior = config.DistributionConfig.DefaultCacheBehavior;
 
-  # Copy index.js file into the header-lambda directory
-  cp src/aws/index.js header-lambda/
+  const lambdaAssociation = behavior.LambdaFunctionAssociations.Items.find(
+    (item) => item.EventType === 'origin-response'
+  );
 
-  # Change to header-lambda directory, zip the index.js file, and move back to the original directory
-  pushd header-lambda
-  zip -q index.zip index.js
-  popd
-}
+  if (!lambdaAssociation) {
+    console.error('No LambdaFunctionAssociation found with EventType "origin-response".');
+    process.exit(1);
+  }
 
-function deploy_lambda() {
-  # Deploy Lambda function
-  aws lambda update-function-code \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --zip-file fileb://header-lambda/index.zip
+  const accountId = execSync('aws sts get-caller-identity --query Account --output text').toString().trim();
+  
+  lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
+    /^arn:aws:lambda:us-east-1:(\d+):/,
+    `arn:aws:lambda:us-east-1:${accountId}:`
+  );
 
-  # Wait for Lambda update to complete
-  sleep 20
+  lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
+    /(:\d+|$)/,
+    `:${lambdaVersion.trim()}`
+  );
 
-  # Publish Lambda version
-  LAMBDA_VERSION=$(aws lambda publish-version \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --query Version \
-    --output text)
+  return config;
+};
 
-  echo "AWS Lambda publish-version output: $LAMBDA_VERSION_OUTPUT"
+const updateDistribution = async (config, distributionId, distributionEtag) => {
+  // Remove unnecessary fields from the configuration object
+  delete config.ETag;
 
-  echo "THE ACCOUNT NUMBERRR: $AWS_ACCOUNT_ID"
-  echo "THE DISTRUBTION ID: $DISTRIBUTION_ID"
-  echo "Lambda function ARN: arn:aws:lambda:us-east-1:$AWS_ACCOUNT_ID:function:$LAMBDA_FUNCTION_NAME:$LAMBDA_VERSION"
-  echo "Lambda function ARN: arn:aws:lambda:us-east-1:$AWS_ACCOUNT_ID:function:$LAMBDA_FUNCTION_NAME:$LAMBDA_VERSION"
+  await fs.writeFile('distribution-config-updated.json', JSON.stringify(config.DistributionConfig, null, 2));
 
-  echo -n $LAMBDA_VERSION
-}
+  execSync(
+    `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
+    { stdio: 'inherit' }
+  );
+};
 
-function update_cloudfront_behavior() {
-  local LAMBDA_VERSION=$1
+const main = async () => {
+  const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
 
-  # Update CloudFront behavior
-  DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
-  DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
-  aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
-  BEHAVIOR_PATH_PATTERN="*"
+  try {
+    const config = await updateCloudFrontBehavior(lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile);
+    await updateDistribution(config, distributionId, distributionEtag);
+  } catch (error) {
+    console.error(`Error updating CloudFront behavior: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-  echo "THE DISTRUBTION ID: $DISTRIBUTION_ID"
-  echo "THE ACCOUNT NUMBERRR: $AWS_ACCOUNT_ID"
-
-  # Call the deploy.mjs script and pass the required arguments
-  node .circleci/deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN" "$(pwd)/distribution-config-original.json" "$AWS_ACCOUNT_ID"
-}
-
-function invalidate_cache_and_sync_s3() {
-  # Invalidate CloudFront cache
-  aws cloudfront create-invalidation \
-    --distribution-id $CLOUDFRONT_DISTRIBUTION_ID \
-    --paths "/*"
-
-  # Sync directory with S3 bucket
-  aws s3 sync dist/circlecitest s3://$S3_BUCKET_NAME --delete
-}
-
-# Main script
-create_lambda_package
-LAMBDA_VERSION=$(deploy_lambda)
-update_cloudfront_behavior "$LAMBDA_VERSION"
-invalidate_cache_and_sync_s3
+main();
