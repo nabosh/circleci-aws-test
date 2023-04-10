17f70da96230ae666eeb81431e8193c3d1c113c0
nabosh <nabosh@gmail.com>
2023-04-10 14:52:07 -0600

    breaking up code some

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -2,40 +2,49 @@ import { promises as fs } from 'fs';
 import { execSync } from 'child_process';
 
 const updateCloudFrontBehavior = async (lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile) => {
+  const configJSON = await fs.readFile(configFile, 'utf-8');
+  const config = JSON.parse(configJSON);
+  const behavior = config.DistributionConfig.DefaultCacheBehavior;
+
+  const lambdaAssociation = behavior.LambdaFunctionAssociations.Items.find(
+    (item) => item.EventType === 'origin-response'
+  );
+
+  if (!lambdaAssociation) {
+    console.error('No LambdaFunctionAssociation found with EventType "origin-response".');
+    process.exit(1);
+  }
+
+  lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
+    /:\d+$/,
+    `:${lambdaVersion}`
+  );
+
+  return config;
+};
+
+const updateDistribution = async (config, distributionId, distributionEtag) => {
+  // Remove unnecessary fields from the configuration object
+  delete config.ETag;
+
+  await fs.writeFile('distribution-config-updated.json', JSON.stringify(config.DistributionConfig, null, 2));
+
+  execSync(
+    `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
+    { stdio: 'inherit' }
+  );
+};
+
+const main = async () => {
+  const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
+  
   try {
-    const configJSON = await fs.readFile(configFile, 'utf-8');
-    const config = JSON.parse(configJSON);
-    const behavior = config.DistributionConfig.DefaultCacheBehavior;
-
-    const lambdaAssociation = behavior.LambdaFunctionAssociations.Items.find(
-      (item) => item.EventType === 'origin-response'
-    );
-
-    if (!lambdaAssociation) {
-      console.error('No LambdaFunctionAssociation found with EventType "origin-response".');
-      process.exit(1);
-    }
-
-    lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
-      /:\d+$/,
-      `:${lambdaVersion}`
-    );
-
-    // Remove unnecessary fields from the configuration object
-    delete config.ETag;
-
-    await fs.writeFile('distribution-config-updated.json', JSON.stringify(config.DistributionConfig, null, 2));
-
-    execSync(
-      `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
-      { stdio: 'inherit' }
-    );
+    const config = await updateCloudFrontBehavior(lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile);
+    await updateDistribution(config, distributionId, distributionEtag);
   } catch (error) {
     console.error(`Error updating CloudFront behavior: ${error.message}`);
     process.exit(1);
   }
 };
 
-const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
-
-updateCloudFrontBehavior(lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile);
+main();
--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 # Deploy Angular App
 
-# Change to the repo directory
-cd ~/repo
+function create_lambda_package() {
+  # Create header-lambda directory
+  mkdir header-lambda
 
-# Create header-lambda directory
-mkdir header-lambda
+  # Copy index.js file into the header-lambda directory
+  cp src/aws/index.js header-lambda/
 
-# Copy index.js file into the header-lambda directory
-cp src/aws/index.js header-lambda/
+  # Change to header-lambda directory, zip the index.js file, and move back to the original directory
+  pushd header-lambda
+  zip -q index.zip index.js
+  popd
+}
 
-# Change to header-lambda directory, zip the index.js file, and move back to the original directory
-pushd header-lambda
-zip -q index.zip index.js
-popd
+function deploy_lambda() {
+  # Deploy Lambda function
+  aws lambda update-function-code \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --zip-file fileb://header-lambda/index.zip
 
-# Deploy Lambda function
-aws lambda update-function-code \
-  --function-name $LAMBDA_FUNCTION_NAME \
-  --zip-file fileb://header-lambda/index.zip
+  # Wait for Lambda update to complete
+  sleep 20
 
-# Wait for Lambda update to complete
-sleep 20
+  # Publish Lambda version
+  LAMBDA_VERSION=$(aws lambda publish-version \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --query Version \
+    --output text)
 
-# Publish Lambda version
-LAMBDA_VERSION=$(aws lambda publish-version \
-  --function-name $LAMBDA_FUNCTION_NAME \
-  --query Version \
-  --output text)
+  echo $LAMBDA_VERSION
+}
 
-# Update CloudFront behavior
-DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
-DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
-aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
-BEHAVIOR_PATH_PATTERN="*"
+function update_cloudfront_behavior() {
+  local LAMBDA_VERSION=$1
 
-# Call the deploy.mjs script and pass the required arguments
-node .circleci/deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN" "$(pwd)/distribution-config-original.json"
+  # Update CloudFront behavior
+  DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
+  DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
+  aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
+  BEHAVIOR_PATH_PATTERN="*"
 
-# Invalidate CloudFront cache
-aws cloudfront create-invalidation \
-  --distribution-id $DISTRIBUTION_ID \
-  --paths "/*"
+  # Call the deploy.mjs script and pass the required arguments
+  node .circleci/deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN" "$(pwd)/distribution-config-original.json"
+}
 
-# Sync directory with S3 bucket
-aws s3 sync dist/circlecitest s3://circleciangularbucket --delete
+function invalidate_cache_and_sync_s3() {
+  # Invalidate CloudFront cache
+  aws cloudfront create-invalidation \
+    --distribution-id $CLOUDFRONT_DISTRIBUTION_ID \
+    --paths "/*"
+
+  # Sync directory with S3 bucket
+  aws s3 sync dist
