db00b3b8089190a2a2399b4862151f53c4f5ce60
nabosh <nabosh@gmail.com>
2023-04-10 21:50:14 -0600

    last deploy WORKED - this is updating the .mjs file (last one did the .sh)

--- .circleci/deploy.mjs
+++ .circleci/deploy.mjs
@@ -1,40 +1,58 @@
+//.circleci/deploy.mjs
 import { promises as fs } from 'fs';
 import { execSync } from 'child_process';
 
-const updateCloudFrontBehavior = async (lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile) => {
-  try {
-    const configJSON = await fs.readFile(configFile, 'utf-8');
-    const config = JSON.parse(configJSON);
-    const behavior = config.DistributionConfig.DefaultCacheBehavior;
+async function getConfigJSON(configFile) {
+  return JSON.parse(await fs.readFile(configFile, 'utf-8'));
+}
 
-    const lambdaAssociation = behavior.LambdaFunctionAssociations.Items.find(
-      (item) => item.EventType === 'origin-response'
-    );
+function findLambdaAssociation(config) {
+  const behavior = config.DistributionConfig.DefaultCacheBehavior;
+  return behavior.LambdaFunctionAssociations.Items.find(
+    (item) => item.EventType === 'origin-response'
+  );
+}
+
+function updateLambdaVersion(lambdaAssociation, lambdaVersion) {
+  lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
+    /:\d+$/,
+    `:${lambdaVersion}`
+  );
+}
+
+async function writeUpdatedConfig(config) {
+  // Remove unnecessary fields from the configuration object
+  delete config.ETag;
+
+  await fs.writeFile('distribution-config-updated.json', JSON.stringify(config.DistributionConfig, null, 2));
+}
+
+function executeCloudFrontUpdate(distributionId, distributionEtag) {
+  execSync(
+    `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
+    { stdio: 'inherit' }
+  );
+}
+
+async function updateCloudFrontBehavior(lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile) {
+  try {
+    const config = await getConfigJSON(configFile);
+    const lambdaAssociation = findLambdaAssociation(config);
 
     if (!lambdaAssociation) {
       console.error('No LambdaFunctionAssociation found with EventType "origin-response".');
       process.exit(1);
     }
 
-    lambdaAssociation.LambdaFunctionARN = lambdaAssociation.LambdaFunctionARN.replace(
-      /:\d+$/,
-      `:${lambdaVersion}`
-    );
+    updateLambdaVersion(lambdaAssociation, lambdaVersion);
+    await writeUpdatedConfig(config);
+    executeCloudFrontUpdate(distributionId, distributionEtag);
 
-    // Remove unnecessary fields from the configuration object
-    delete config.ETag;
-
-    await fs.writeFile('distribution-config-updated.json', JSON.stringify(config.DistributionConfig, null, 2));
-
-    execSync(
-      `aws cloudfront update-distribution --id ${distributionId} --if-match ${distributionEtag} --distribution-config file://distribution-config-updated.json`,
-      { stdio: 'inherit' }
-    );
   } catch (error) {
     console.error(`Error updating CloudFront behavior: ${error.message}`);
     process.exit(1);
   }
-};
+}
 
 const [_, __, lambdaVersion, distributionId, distributionEtag, behaviorPathPattern, configFile] = process.argv;
 
