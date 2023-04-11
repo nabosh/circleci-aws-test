f965f70405ed92b84b0ebe88983b6d65be53639e
nabosh <nabosh@gmail.com>
2023-04-10 21:44:34 -0600

    last deploy WORKED - this is trying to make it more readable and modular

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,47 +1,68 @@
+#.circleci/deploy.sh
 #!/bin/bash
+set -e
 # Deploy Angular App
 
-# Change to the repo directory
-cd ~/repo
+function create_lambda_directory() {
+  # Create header-lambda directory
+  mkdir header-lambda
 
-# Create header-lambda directory
-mkdir header-lambda
+  # Copy index.js file into the header-lambda directory
+  cp src/aws/index.js header-lambda/
+}
 
-# Copy index.js file into the header-lambda directory
-cp src/aws/index.js header-lambda/
+function zip_lambda() {
+  # Change to header-lambda directory, zip the index.js file, and move back to the original directory
+  pushd header-lambda
+  zip -q index.zip index.js
+  popd
+}
 
-# Change to header-lambda directory, zip the index.js file, and move back to the original directory
-pushd header-lambda
-zip -q index.zip index.js
-popd
+function deploy_lambda() {
+  # Deploy Lambda function
+  aws lambda update-function-code \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --zip-file fileb://header-lambda/index.zip
+}
 
-# Deploy Lambda function
-aws lambda update-function-code \
-  --function-name $LAMBDA_FUNCTION_NAME \
-  --zip-file fileb://header-lambda/index.zip
+function publish_lambda_version() {
+  # Wait for Lambda update to complete
+  sleep 20
 
-# Wait for Lambda update to complete
-sleep 20
+  # Publish Lambda version
+  aws lambda publish-version \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --query Version \
+    --output text
+}
 
-# Publish Lambda version
-LAMBDA_VERSION=$(aws lambda publish-version \
-  --function-name $LAMBDA_FUNCTION_NAME \
-  --query Version \
-  --output text)
+function update_cloudfront_behavior() {
+  local LAMBDA_VERSION=$1
 
-# Update CloudFront behavior
-DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
-DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
-aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
-BEHAVIOR_PATH_PATTERN="*"
+  # Update CloudFront behavior
+  DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
+  DISTRIBUTION_ETAG=$(aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --query ETag --output text)
+  aws cloudfront get-distribution-config --id $DISTRIBUTION_ID --output json > distribution-config-original.json
+  BEHAVIOR_PATH_PATTERN="*"
 
-# Call the deploy.mjs script and pass the required arguments
-node .circleci/deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN" "$(pwd)/distribution-config-original.json"
+  # Call the deploy.mjs script and pass the required arguments
+  node .circleci/deploy.mjs "$LAMBDA_VERSION" "$DISTRIBUTION_ID" "$DISTRIBUTION_ETAG" "$BEHAVIOR_PATH_PATTERN" "$(pwd)/distribution-config-original.json"
+}
 
-# Invalidate CloudFront cache
-aws cloudfront create-invalidation \
-  --distribution-id $DISTRIBUTION_ID \
-  --paths "/*"
+function invalidate_cache_and_sync_s3() {
+  # Invalidate CloudFront cache
+  aws cloudfront create-invalidation \
+    --distribution-id $CLOUDFRONT_DISTRIBUTION_ID \
+    --paths "/*"
 
-# Sync directory with S3 bucket
-aws s3 sync dist/circlecitest s3://circleciangularbucket --delete
+  # Sync directory with S3 bucket
+  aws s3 sync dist/circlecitest s3://circleciangularbucket --delete
+}
+
+# Main script
+create_lambda_directory
+zip_lambda
+deploy_lambda
+LAMBDA_VERSION=$(publish_lambda_version)
+update_cloudfront_behavior "$LAMBDA_VERSION"
+invalidate_cache_and_sync_s3
